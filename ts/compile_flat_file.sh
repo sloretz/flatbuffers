c00aa6b2432b7bed861fc7b9f1de17472a24aa0a
Shane Loretz <sloretz@intrinsic.ai>
2026-07-14 19:23:49 +0000

    Comments about why these script changes are necessary
    Signed-off-by: Shane Loretz <sloretz@intrinsic.ai>

--- ts/compile_flat_file.sh
+++ ts/compile_flat_file.sh
@@ -2,8 +2,8 @@
 # This is a script used by the typescript flatbuffer bazel rules to compile
 # a flatbuffer schema (.fbs file) to typescript and then use esbuild to
 # generate a single output.
-# Note: This relies on parsing the stdout of flatc to figure out how to
-# run esbuild.
+# Note: This uses flatc --file-names-only to determine the generated main entry
+# point file to pass to esbuild.
 # --- begin runfiles.bash initialization v2 ---
 # Copy-pasted from the Bazel Bash runfiles library v2.
 set -uo pipefail; set +e; f=bazel_tools/tools/bash/runfiles/runfiles.bash
@@ -17,9 +17,12 @@ source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
 set -eu
 runfiles_export_envvars
 FLATC=$(rlocation com_github_google_flatbuffers/flatc)
+# The schema file (.fbs) is the last argument passed to flatc ($@).
 FBS_FILE="${!#}"
 SCHEMA_BASE=$(basename "${FBS_FILE}" .fbs)
 ${FLATC} "$@"
+# flatc no longer prints generated entry point paths to console output when
+# compiling. Use --file-names-only to discover the generated .ts entry point.
 TS_FILE=$(${FLATC} --file-names-only "$@" 2>/dev/null | grep "/${SCHEMA_BASE}\(_[a-zA-Z0-9_-]*\)\?\.ts$" | head -n 1)
 export PATH="$(rlocation nodejs_linux_amd64/bin/nodejs/bin):${PATH}"
 ${ESBUILD_BIN} "${TS_FILE}" --format=cjs --bundle --outfile="${OUTPUT_FILE}"  --external:flatbuffers --log-level=warning
